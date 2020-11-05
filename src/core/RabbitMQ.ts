24092366b3b5f0c7aaaafefc986230d1f228172e
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-11-06 00:27:24 +0200

    [RabbitMQ] Added debug logs.

--- src/core/RabbitMQ.ts
+++ src/core/RabbitMQ.ts
@@ -11,7 +11,6 @@
 import * as Amqp from "amqplib";
 import {AMQP_REPORTS_QUEUE, AMQP_TASKS_QUEUE} from "../globals";
 import RenderDispatcher from "./RenderDispatcher";
-import set = Reflect.set;
 
 export type ReportTypes = "info" | "warning" | "error";
 
@@ -22,7 +21,6 @@ export interface SendTaskReportSettings {
 export default class RabbitMQ {
     public static connection: Amqp.Connection;
     public static renderTasksChannel: Amqp.Channel;
-    public static taskReportConnection: Amqp.Channel;
 
 
     public constructor(config: Amqp.Options.Connect) {
@@ -34,7 +32,6 @@ export default class RabbitMQ {
     public static async init(config: Amqp.Options.Connect): Promise<void> {
         RabbitMQ.connection = await Amqp.connect(config);
         RabbitMQ.renderTasksChannel = await RabbitMQ.connection.createChannel();
-        RabbitMQ.taskReportConnection = await RabbitMQ.connection.createChannel();
 
         // Consuming render task
         await RabbitMQ.renderTasksChannel.assertQueue(AMQP_TASKS_QUEUE);
@@ -43,17 +40,14 @@ export default class RabbitMQ {
             console.log("Got render task from queue.");
             let renderTask: any = null;
             try {
+                RabbitMQ.renderTasksChannel.ack(message);
                 renderTask = JSON.parse(message.content.toString());
-            } catch (error) {
-                // TODO: handle invalid message.
-            }
-            try {
                 await this.sendTaskStartReport(renderTask.id);
                 await RenderDispatcher.doRenderTask(renderTask);
-                RabbitMQ.renderTasksChannel.ack(message);
                 await this.sendTaskFinishReport(renderTask.id);
                 // TODO: Add result to queue.
             } catch(error) {
+                RabbitMQ.renderTasksChannel.nack(message);
                 await this.sendTaskFinishReport(renderTask.id, "failed", {message: error.message});
             }
         });
@@ -70,12 +64,13 @@ export default class RabbitMQ {
             task: taskId,
             data: message,
         }
-
         console.log("REPORT PAYLOAD -----------------------------------------------------");
         console.log(payload);
         console.log("REPORT PAYLOAD END -------------------------------------------------");
-        await RabbitMQ.taskReportConnection.assertQueue(AMQP_REPORTS_QUEUE);
-        await RabbitMQ.taskReportConnection.sendToQueue(AMQP_REPORTS_QUEUE, Buffer.from(JSON.stringify(payload)));
+        const channel: Amqp.Channel = await RabbitMQ.connection.createChannel();
+        await channel.assertQueue(AMQP_REPORTS_QUEUE);
+        await channel.sendToQueue(AMQP_REPORTS_QUEUE, Buffer.from(JSON.stringify(payload)));
+        await channel.close();
     }
 
     protected  static async sendTaskStartReport(taskId: number): Promise<void> {
