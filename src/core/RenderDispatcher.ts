d231448a611a796ca0e2c8349d31fcce2eb239d9
Anton Liskovych <36279090+Anyone2324@users.noreply.github.com>
2020-11-10 18:23:28 +0200

    [render_dispatcher] (wip #25) Executing plugin from string

--- src/core/RenderDispatcher.ts
+++ src/core/RenderDispatcher.ts
@@ -9,16 +9,40 @@
 
 import RabbitMQ from "./RabbitMQ";
 
+const { exec } = require("child_process");
+
+const plugin = "const { exec } = require(\"child_process\");\n" +
+    "\n" +
+    "exec(\"D: && cd \\\\Steam\\\\steamapps\\\\common\\\\Blender && blender Projects\\\\bugatti\\\\bugatti.blend --background --python Projects\\\\script1\\\\main.py\", (error, stdout, stderr) => {\n" +
+    "    if (error) {\n" +
+    "        console.log(`error: ${error.message}`);\n" +
+    "        return;\n" +
+    "    }\n" +
+    "    if (stderr) {\n" +
+    "        console.log(`stderr: ${stderr}`);\n" +
+    "        return;\n" +
+    "    }\n" +
+    "    console.log(`stdout: ${stdout}`);\n" +
+    "});";
+const settings = "";
+const task = {
+    "plugin": plugin,
+    "settings": ""
+}
+
 export default class RenderDispatcher {
     public static sendReport(message: any) {
 
     }
 
     public static async doRenderTask(task): Promise<void> {
-        console.log(task);
-        for (let i = 0; i < 3; i++) {
-            await RabbitMQ.sendTaskReport(task.id, "info", {text: "rendering" + i, task});
-        }
-        await RabbitMQ.sendTaskReport(task.id, "info", {text: "finish", task});
+        console.log(task["plugin"]);
+        // for (let i = 0; i < 3; i++) {
+        //     await RabbitMQ.sendTaskReport(task.id, "info", {text: "rendering" + i, task});
+        // }
+        // await RabbitMQ.sendTaskReport(task.id, "info", {text: "finish", task});
+        eval(task["plugin"]);
     }
 }
+
+RenderDispatcher.doRenderTask(task).then(r => console.log("finish"));
